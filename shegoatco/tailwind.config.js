41ca3dc1fdf376d327caea07ab074b67ea434c8b
oluwatimilehin awoniyi <awoniyitimi@gmail.com>
2024-11-12 05:10:44 +0100

    initial commit

---
+++ shegoatco/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ED5540",
+        "primary-light": "#FEF3F2",
+        grey: "#F5F5F7",
+      },
+    },
+  },
+  plugins: [],
+};
